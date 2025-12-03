16cf28e1310027c65d2a3e43caa5000276c34ce0
dp <dp@dpg06.top>
2025-12-03 23:43:24 +0800

    add transactions database to sql file

--- devtools/createdatabase.sql
+++ devtools/createdatabase.sql
@@ -7,3 +7,16 @@ CREATE TABLE user (
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    userid INT NOT NULL,
+    date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    amount DECIMAL(10, 2) NOT NULL,
+    type VARCHAR(20) NOT NULL,
+    description VARCHAR(255),
+    categoryId INT,
+    FOREIGN KEY (userid) REFERENCES user(uid),
+    INDEX (userid, type),
+    INDEX (date)
+);
